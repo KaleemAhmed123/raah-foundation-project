7783ff9e48311b6aa429d2e98c77f4c9d839e561
KaleemAhmed123 <shaamidreez@gmail.com>
2023-04-15 10:52:13 +0530

    project structure done.

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import Home from "./home/Home";
+import About from "./About/About";
+import Donate from "./donate/Donate";
+import Gallery from "./gallery/Gallery";
+import Join from "./join/Join";
+import Vision from "./vision/Vision";
+import NotFound from "./notFound/NotFound";
+
+export { Home, About, Donate, Gallery, Join, Vision, NotFound };
---
+++ src/pages/join/Join.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./join.css";
+
+const Join = () => {
+  return <div>Join</div>;
+};
+
+export default Join;
